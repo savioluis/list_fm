380be50b097bae8099c961e857e54d5f6e29930b
savioluis <savioluislobo@gmail.com>
2025-08-28 16:30:26 -0300

    feat(data): add user remote Data Source

---
+++ lib/features/search_user/data/data_sources/remote/user_remote_data_source.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:list_fm/features/search_user/data/models/user_model.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'user_remote_data_source.g.dart';
+
+@RestApi()
+abstract class UserRemoteDataSource {
+  factory UserRemoteDataSource(Dio dio) = _UserRemoteDataSource;
+
+  @GET('')
+  Future<HttpResponse<UserModel>> getUserByName({
+    @Query('method') method = 'user.getinfo',
+    @Query('user') String? name,
+  });
+}
