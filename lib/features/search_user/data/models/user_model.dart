a7ea250038530fae0fe63e0b913693f26cdab52e
savioluis <savioluislobo@gmail.com>
2025-08-29 16:13:41 -0300

    feat: add memberSince field to user Model and Entity

--- lib/features/search_user/data/models/user_model.dart
+++ lib/features/search_user/data/models/user_model.dart
@@ -1,13 +1,17 @@
+import 'package:list_fm/core/util/date_time_util.dart';
 import 'package:list_fm/features/search_user/domain/entities/user_entity.dart';
 
 class UserModel extends UserEntity {
   const UserModel({
     super.name,
     super.imageUrl,
+    super.memberSince,
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     final Map<String, dynamic> currentUser = json['user'];
+
+    final String userName = currentUser['name'];
 
     final List<dynamic> images = currentUser['image'];
 
@@ -16,9 +20,13 @@ class UserModel extends UserEntity {
       orElse: () => null,
     )['#text'];
 
+    final memberSinceDateTime = DateTimeUtil.fromUnixTime(currentUser['registered']['#text']);
+    final memberSinceString = '${DateTimeUtil.getMonthName(memberSinceDateTime.month)} ${memberSinceDateTime.year}';
+
     return UserModel(
-      name: currentUser['name'],
+      name: userName,
       imageUrl: mediumImageUrl,
+      memberSince: memberSinceString,
     );
   }
 }
--- lib/features/search_user/domain/entities/user_entity.dart
+++ lib/features/search_user/domain/entities/user_entity.dart
@@ -3,15 +3,18 @@ import 'package:equatable/equatable.dart';
 class UserEntity extends Equatable {
   final String? name;
   final String? imageUrl;
+  final String? memberSince;
 
   const UserEntity({
     this.name,
     this.imageUrl,
+    this.memberSince,
   });
 
   @override
   List<Object?> get props => [
     name,
     imageUrl,
+    memberSince,
   ];
 }
