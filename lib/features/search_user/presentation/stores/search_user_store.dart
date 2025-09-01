893d863b90964af2e5f4649e3e06b4ed7db4b7a6
savioluis <savioluislobo@gmail.com>
2025-09-01 12:06:02 -0300

    feat(store): enhance fetchUser logic to handle loading states and user duplication

--- lib/features/search_user/presentation/stores/search_user_store.dart
+++ lib/features/search_user/presentation/stores/search_user_store.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
+
 import 'package:list_fm/core/network/network_data_state.dart';
-import 'package:list_fm/features/search_user/domain/entities/user_entity.dart';
-import 'package:list_fm/features/search_user/domain/usercases/get_user_usecase.dart';
+import 'package:list_fm/features/search_user/domain/usercases/search_user_usecase.dart';
 import 'package:list_fm/features/search_user/presentation/stores/search_user_state.dart';
 import 'package:mobx/mobx.dart';
 
@@ -22,18 +23,49 @@ abstract class _SearchUserStore with Store {
   Future<void> fetchUser(String username) async {
     final currentState = state;
 
-    state = SearchUserLoadingState();
+    if (currentState is SearchUserSuccessState) {
+      state = currentState.copyWith(isLoadingMore: true);
+    } else {
+      state = SearchUserLoadingState();
+    }
 
     final result = await _getUserUsecase.call(params: username);
 
     if (result is NetworkDataSuccess) {
-      state = SearchUserSuccessState(
-        users: [...currentState.users ?? [], ?result.data],
-      );
+      if (currentState is SearchUserSuccessState) {
+        final userAlreadyFetched = currentState.users!.any((user) => user.name == username);
+
+        userAlreadyFetched
+            ? state = currentState.copyWith(userAlreadyFetched: true)
+            : state = SearchUserSuccessState(
+                users: [...currentState.users ?? [], ?result.data],
+                searchNotFound: false,
+                userAlreadyFetched: false,
+                isLoadingMore: false,
+              );
+        return;
+      } else if (currentState is SearchUserInitialState || currentState is SearchUserLoadingState) {
+        state = SearchUserSuccessState(
+          users: [...currentState.users ?? [], ?result.data],
+          searchNotFound: false,
+        );
+        return;
+      }
     } else if (result is NetworkDataFailure) {
-      state = SearchUserFailureState(
-        error: result.error,
-      );
+      final isNotFoundError = (result.error!.response?.data["error"] == 6);
+
+      if (currentState is SearchUserSuccessState) {
+        if (isNotFoundError) {
+          state = currentState.copyWith(searchNotFound: true);
+          return;
+        }
+      } else {
+        if (isNotFoundError) {
+          state = SearchUserInitialState(searchNotFound: true);
+        } else {
+          state = SearchUserFailureState(error: result.error);
+        }
+      }
     }
   }
 }
